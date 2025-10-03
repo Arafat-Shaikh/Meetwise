b1455027e891c648ac3ccbc78678ac37743842c7
arafat <arafatshaikh823@gmail.com>
2025-10-03 17:45:37 +0530

    removed middleware

--- middleware.ts
+++ middleware.ts
@@ -36,9 +36,9 @@ export async function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/__next_action|api/__next_internal).*)",
-    "/api/(.*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/((?!_next/static|_next/image|favicon.ico|api/__next_action|api/__next_internal).*)",
+//     "/api/(.*)",
+//   ],
+// };
